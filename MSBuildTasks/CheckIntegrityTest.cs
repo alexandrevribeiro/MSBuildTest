d1df8f4fc86a1ab52990342756702a242e778ca5
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-05 00:04:32 -0300

    First commit (not working yet)

---
+++ MSBuildTasks/CheckIntegrityTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace MSBuildTasks
+{
+    public class CheckIntegrityTask : Task
+    {
+
+        [Required]
+        public bool ShowError { get; set; }
+
+        [Output]
+        public string SuccessMessage { get; set; }
+
+        // Reference: http://thomasardal.com/msbuild-tutorial/ 
+        public override bool Execute()
+        {
+            //throw new InvalidOperationException();
+            if (ShowError)
+            {
+                Log.LogError("An error has occurred!!!");
+                return false;
+            }
+
+            SuccessMessage = "Integrity check successufuly made!";
+            return true;
+        }
+    }
+}
